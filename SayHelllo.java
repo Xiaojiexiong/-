416951c929a65ce55356a040e9fce920174ff592
Xiaojiexiong <you@example.com>
2019-09-09 23:44:35 +0800

    create a sayHello class

---
+++ SayHelllo.java
@@ -0,0 +1,10 @@
+public class SayHello {
+    public static void main(String[] args) {
+	    System.out.printl("Selow is the fuction of sayhello");
+		sayHello(0);
+	}
+	
+	public static void sayHello(int sum){
+	    System.out.println("say Hello!"+sum);
+	}
+}
