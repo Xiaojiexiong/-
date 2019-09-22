30fa4ce9302245131686f900fcb0ca9dd16ede14
Xiaojeixiong <you@example.com>
2019-09-22 17:22:50 +0800

    craete a Page30No2 class

---
+++ Page30No2.java
@@ -0,0 +1,10 @@
+public class Page30No2 {
+	public static void main(String[] args) {
+		int count = 10;
+		count *= count + 2;
+		count /= 100;
+		count ++;
+		System.out.printf("%d",count);
+	}
+		
+}
