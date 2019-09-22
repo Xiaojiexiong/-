407c1ada0435490425550ef6035be89b017a3073
Xiaojeixiong <you@example.com>
2019-09-22 19:51:41 +0800

    create a Page30No3 class

---
+++ Page30No3.java
@@ -0,0 +1,28 @@
+public class Page30No3 {
+	public static void main(String[] args) {
+		int res,n1 = 12,n2 = 24,n3 = 16,n4 = 3;
+		double d,v1 = 10.0,v2 = 3.1416;
+		res = n1/n4;
+		System.out.printf("res =n1/n4 = %d  ",res);
+		d = n1/n4;
+		System.out.printf("d =n1/n4 = %.2f  ",d);
+		res = n3/n4;
+		System.out.printf("res =n3/n4 = %d  ",res);
+		d = n3/n4;
+		System.out.printf("d = n3/n4 = %.2f  ",d);
+		d = v1/n4;
+		System.out.printf("d = v1/n4 = %.2f  ",d);
+		d = v1/v2;
+		System.out.printf("d =n1/n4 = %.2f  ",d);
+		res = n1/n2;
+		System.out.printf("res =n1/n2 = %d  ",res);
+		d = (double)n1/n2;
+		System.out.printf("d  =(double)n1/n2 = %.2f  ",d);
+		d = n1/(double)n2;
+		System.out.printf("d =n1/(double)n2 = %.2f  ",d);
+		d = (double)(n1/n2);
+		System.out.printf("d =(double)(n1/n2) = %.2f  ",d);
+	}	
+}
+	
+		
